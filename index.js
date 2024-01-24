22df780fd9570d5b660adf293010744bd1e05c13
arana971 <130342534+arana971@users.noreply.github.com>
2024-01-24 14:23:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var randomImages = "dice" + randomNumber + ".png";
+var randomImagesSource = "images/" + randomImages;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImagesSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImagesSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImagesSource2);
+
+if(randomNumber > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomNumber2 > randomNumber){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
